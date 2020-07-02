e8ae631e6386b3af9e72efa392448e982769b80a
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2020-07-02 16:20:35 +0600

    Add files via upload

---
+++ TopSort.cpp
@@ -0,0 +1,43 @@
+//O(Nodes+Edges)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define  CIN            ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define  ll             long long int
+vector<ll>v1[10005];
+void topsort(ll node)
+{
+    ll i ,cnt=0,indegree[node+2]={0};
+    for( i = 1 ; i<= node ; i++ )
+        for( auto v: v1[i]) indegree[v]++;
+    queue<ll>qu;
+    for( i = 1 ; i<= node ; i++)
+        if(!indegree[i]) qu.push(i);
+    vector<ll>ans;
+    while(!qu.empty())
+    {
+        ll id = qu.front();
+        qu.pop();
+        ans.push_back(id);
+        for( auto v:v1[id]){
+            if(--indegree[v]==0) qu.push(v);
+        }
+        cnt++;
+    }
+    if(cnt!=node) {cout<<"Sandro fails."<<endl;return;}
+    for( i = 0 ; i < node ; i++) cout<<ans[i]<<" ";
+    cout<<endl;
+}
+int main()
+{
+    CIN;
+    ll  node,edges,u,v,i;
+    cin >> node >> edges;
+    for( i =1 ; i<= edges ; i++)
+    {
+        cin >> u >> v ;
+        v1[u].push_back(v);
+    }
+    topsort(node);
+    return 0 ;
+}
