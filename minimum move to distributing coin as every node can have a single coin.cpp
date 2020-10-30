320ec0cff46d3d4fc73810a5ff472e4fab648d06
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2020-10-30 07:05:12 +0600

    Add files via upload

---
+++ minimum move to distributing coin as every node can have a single coin.cpp
@@ -0,0 +1,10 @@
+int traverse(TreeNode* r, int &moves) {
+  if (r == nullptr) return 0;
+  int left = traverse(r->left, moves), right = traverse(r->right, moves);
+  moves += abs(left) + abs(right);
+  return r->val + left + right - 1;
+}
+int distributeCoins(TreeNode* r, int moves = 0) {
+  traverse(r, moves);
+  return moves;
+}
