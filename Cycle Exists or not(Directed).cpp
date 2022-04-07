09a2ff2fdf99874815ffce2c0377c400117b3cc1
Kamrul_Hasan <48267671+Kamrul-Hasan-1971@users.noreply.github.com>
2022-04-07 16:41:08 +0600

    Update Cycle Exists or not(Directed).cpp

--- Cycle Exists or not(Directed).cpp
+++ Cycle Exists or not(Directed).cpp
@@ -1,28 +1,40 @@
-void cycle(int v,vector<int>v1[],bool &ck,int color[])
+class Solution
 {
-    color[v] = 1;
-    for(int w : v1[v])
+public:
+    bool cycleDirected(int v,vector<int>v1[], int color[])
     {
-        if(color[w] == 1) ck=1;
-        if(ck) return ;
-        if(color[w] == 0) cycle(w,v1,ck,color);
-    }
-    color[v] = 2;
-}
-bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
-{
-    vector<int>v1[numCourses+1];
-    int color[numCourses];
-    for(int i = 0 ; i< numCourses; i++)color[i]=0;
-    bool ck=0;
-    for(auto x : prerequisites ) v1[x[0]].push_back(x[1]);
-    for( int i = 0 ; i <numCourses ; i++ )
-    {
-        if(color[i]==0)
+        color[v] = 1;
+        for(int w : v1[v])
         {
-            cycle(i,v1,ck,color);
-            if(ck) return false;
+            if(color[w] == 1) return true;
+            if(color[w] == 0 && cycle(w,v1,color)) return true;
         }
+        color[v] = 2;
+        return false;
     }
-    return true;
-}
+
+    bool cycleUnDirected(int v,int parent,vector<int>v1[], int color[])
+    {
+        color[v] = 1;
+        for(int w : v1[v])
+        {
+            if(w == parent) continue;
+            if(color[w]) return true;
+            if(cycle(w,v,v1,color)) return true;
+        }
+        return false;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
+    {
+        vector<int>v1[numCourses+1];
+        int color[numCourses];
+        for(int i = 0 ; i< numCourses; i++)color[i]=0;
+        bool ck=0;
+        for(auto x : prerequisites ) v1[x[0]].push_back(x[1]);
+        for( int i = 0 ; i <numCourses ; i++ )
+            if(color[i]==0 && cycle(i,v1,color)) return false; // directed
+            if(color[i]==0 && cycle(i,-1,v1,color)) return false; //undirected
+        return true;
+    }
+};
